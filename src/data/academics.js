f5e45594533db6a00e06732f9d44525f5d39edb2
shaurya-w <shauryawajge@gmail.com>
2026-01-22 22:52:20 +0530

    modified content

--- src/data/academics.js
+++ src/data/academics.js
@@ -82,4 +82,68 @@ Subject: WORKSHOP AND UHV – 25 marks each (no theory exam)
 
 `
   },
+
+  {
+    id: 5678,
+    slug: "semester-3-overview",
+    title: "Semester 3 Overview",
+    subtitle: "First steps into CS",
+    date: "December 22, 2025",
+    content: `
+-> Subject: ANALYSIS OF ALGORITHMS
+
+This subject, I feel, was one of the most important subjects of this semester as it covered all the fundamentals of writing algorithms and a few most common algorithms as well. It started right from what is time and space complexity and went from basic sorting algorithms to introducing us to Greedy and Dynamic Programming algorithms as well. Including string matching algorithms; it was interesting to think that all of this goes on behind the scenes when you run a Ctrl + F search on a document.
+
+**Thinking about the evolution of a solution from brute force to an optimal approach was something new**. I feel brute force is the solution that comes up to you intuitively but it isn’t necessarily the most efficient. Learning about the optimized versions of those for the first time makes you wonder how anyone even came up with these approaches in the first place!
+
+The lectures were followed by practical sessions where we were asked to code the algorithms. Each weekly practical session ended with a viva, which kept us on our toes. This is where the “analysis” in analysis of algorithm came alive, we were asked everything from time and space complexities to applications to what would happen in the best, average and worst case scenarios. It was quite a rollercoaster.
+
+For a junior reading this, if there’s any takeaway you get from this blog is that Abdul Bari sir on Youtube is a lifesaver when it comes to learning algorithms and understanding dry-runs.
+
+For reference, you can find all the lab implementations here:
+https://github.com/shaurya-w/AOA_codes/
+
+-> Subject: COMPUTER ORGANIZATION AND ARCHITECTURE
+
+This one was highly theoretical for the most part. Writing assembly language programs for the lab though was quite a task. It gave me newfound appreciation for high-level languages built on top of assembly.
+
+-> Subject: DISCRETE STRUCTURES AND GRAPH THEORY
+
+The modules included **sets, relations, functions, algebraic structures, and graph theory**. For the most part, it was easy. The end-semester exam, however, was one of those papers that makes you wonder whether you’re sitting in the right exam hall or if you somehow missed an entire portion of the syllabus.
+
+The concepts themselves were familiar, but the questions were framed very differently compared to previous year papers. Almost every question focused on applications, and it felt like, *“Wait, hold up a minute…I think I know this”* Except there was a lack of sufficient minutes :)
+
+Overall, it was challenging but doable. The biggest lesson here was that **conceptual understanding will always save you**, especially when papers don’t follow predictable patterns. Either way, the university always seems to have at least one outlier paper every semester.
+
+-> Subject: FULL STACK JAVA PROGRAMMING
+
+For starters, I’m glad this was introduced as a subject. The modules covered an introduction to Java and core concepts like Object-Oriented Programming. We also had HTML, CSS, JavaScript, and React bundled into a single module. I was already familiar with these topics, which definitely helped.
+
+The final module focused on Spring and Spring Boot and an introduction to MVC architecture, though we didn’t get as much time to explore it towards the end of the semester.
+
+The labs were alright (and yes, we had viva for those too). This was also the first time we worked on a mini project that had to be presented at the end of the semester. Perhaps more on that later in another blog.
+
+-> Subject: MATHEMATICS III
+
+M3 was a mix of **linear algebra, fourier series, statistics, probability, and linear programming.** A lot of it revolved around matrices, vectors, and solving systems of equations, along with some probability and stats.
+
+Linear programming was probably the most practical part of the syllabus. It was all about finding the *best possible outcome* given a set of constraints, which makes sense conceptually. That said, actually solving these problems on paper could get pretty long and repetitive.
+
+Overall, this subject was more about **practice and patience** than anything else.
+
+-> Subject: OPEN SOURCE TECHNOLOGIES (Open Elective)
+
+Just because source code is publicly available doesn’t mean you can do whatever you want with it. In this course, we learnt about different kinds of software licenses and the do’s and don’ts, and the can’s and can’ts that these licenses specify.
+
+Throughout the semester, we worked with **Linux**. As a Windows user myself, it was quite a learning experience, and I’m glad we had this exposure because, at some point, we might need to work with Linux as well.
+
+Honestly, I didn’t know you could set up a **virtual environment** with another operating system on your computer. It borrows resources like CPU, RAM, and storage with the host OS, but yes, it is very much possible.
+
+Every week, with each lab, we were figuring out how to install new software and applications and work with them. The syllabus mainly covered **LibreOffice**, **Blender**, **Docker**, **Apache**, **Git**, **GitHub**, and even image editing software like **GIMP**. It was good exposure and helpful in understanding some widely used open-source tools.
+
+Final Note
+Overall, this semester was a mix of theory, practicals, viva stress, and genuine learning. Some subjects demanded conceptual clarity, some tested patience, and some made you appreciate how far modern tools have come. Definitely one of the more memorable semesters so far.
+`
+  }
+
 ]; 
