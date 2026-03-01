2ad9fced0ec7dc4e27aca223cf750358407d6f59
shaurya-w <shauryawajge@gmail.com>
2026-03-01 14:20:18 +0530

    added new content - 28-2-26

--- src/data/tech.js
+++ src/data/tech.js
@@ -1,41 +1,101 @@
 export const blogs = [
-  {
-    id: 1234,
-    slug: "a-blog-about-a-blog",
-    title: "A blog about a blog",
-    subtitle: "Because...why not?",
-    date: "August 21, 2025",
-    content: `
-I’d always wanted to start a blog. Lousy Writers first began as a WordPress site, but I soon discovered that I could build a website myself, and since it’s now part of my curriculum, that turned out to be a win-win situation. So, I began learning web technologies to create this website. Along the way, I discovered that there are two aspects of web development: frontend and backend. Most people begin with frontend, which is about how the website looks and how users interact with it (excluding functionality, which is essentially part of backend development).
+   {
+  id: 9101,
+  slug: "how-are-passwords-stored-on-the-internet",
+  title: "Encoding, Encryption, Hashing",
+  subtitle: "How are passwords stored on the internet?",
+  date: "February 28, 2026",
+  content: `
+As I was learning about authentication and how passwords are stored, I came across these concepts which I found really interesting. So naturally, I had to share them.
 
--> Design, Setup, and Tailwind
-I started by designing the basic layout of the website for both mobile and desktop views. You know, there’s something deeply calming about fine-tuning a design down to the very last pixel. I had studied plain CSS earlier, and I decided to learn Tailwind this time.
 
-And thus began the flood of npm commands — and the inevitable “command not found” errors — during the Tailwind installation. Not to mention the bulky and intimidating node_modules folder.
+# Encoding
 
-I worked through the Tailwind docs, experimenting with CSS properties until I felt comfortable. Then I coded the homepage in desktop view — only to realize later that Tailwind is built on a mobile-first philosophy. You’re supposed to code the mobile version first, then tweak it for larger screens using responsive variants like sm:, md:, and lg:. So I had to redesign it, and that was a lesson learned the hard way.
+Encoding is basically converting data into another format so that it can be safely transmitted or stored.
 
--> React, Components, and Routing
-After getting comfortable with styling, the next step was learning how to build reusable components instead of rewriting the same code repeatedly. This meant shifting from blindly typing out code to thinking carefully about whether a div could be reused, how it would fit into the component hierarchy, and which parts should stay consistent versus which parts needed to change.
+Encoding is not used for security. It simply transforms data to a standard format for transmission.
 
-For example, the blog cards on this site share the same structure and design, but the actual content - title, subtitle, and dates differ. That’s where Props come in — they allow you to pass data into components so they stay reusable. It’s similar to writing a function and then calling it with different arguments. 
+A common example is Base64 encoding.
 
-On the other hand, when data changes inside a component (like the value of a counter or a like button), that’s handled with State. State is like the memory of a component — it stores values that can change over time as the user interacts with it.
+For example:
 
-To set up routing and dynamic URLs, I used React Router Dom. Notice how the page doesn’t reload when you navigate to another page? That’s because React Router DOM enables client-side routing in single-page applications, which allows smoother user interactions and faster loading times. 
+Hello → SGVsbG8=
 
-In short, I really appreciate how thoughtfully React was designed — a simple yet powerful tool.
+The important thing to understand is that encoding is reversible and anyone can decode encoded data without needing a secret key. Interestingly enough, we did learn about this in our Math class last semester.
 
--> Storage, GitHub, and Deployment
-Currently, as I write this, the blog contents are saved in local storage files. But I do have a few options in mind that I can later switch to, partly because the formatting of the blog content on this page that you're reading can be improved. Not to mention the possibility of adding images in the future. 
+Encoding is mainly used for:
 
-Learning Git and GitHub through this project has been incredibly fruitful. I've gained a practical understanding of what each command does, and more importantly, the 'why' behind using them. When you first learn about them, they all feel so theoretical and abstract, like "What do you even mean I've got a merge conflict from GitHub?!" The only conflict I sensed was cognitive, trying to merge these new concepts into my understanding! But all of that eventually got better.
+- Formatting binary data into text  
+- Sending files or images through APIs  
+- Safely transmitting data through formats like JSON over HTTP  
 
-I thought deploying your website had a steep learning curve and would take a lot of time, and once you've deployed, nothing could be changed. But I was glad to be mistaken. One of my friends told me deploying on Vercel takes up no more than 5 minutes, since it pulls the code directly from your GitHub repository.  So — dramatic pause — I finally deployed the project and opened the link on my phone. That moment felt really great (after fixing a few responsiveness issues, obviously). But in hindsight, I think that’s development in a nutshell: thinking you’re completely stuck until suddenly, you’re not.
+For example, when binary data (like images or files) needs to be included inside JSON responses, it is often Base64 encoded so it can be transmitted as text. Because encoding is easily reversible, it should never be used to store passwords or protect sensitive information.
+
+
+# Hashing
+
+Hashing converts data into a fixed-length string using mathematical functions.
+
+Unlike encoding, hashing is designed to be one-way. Speaking in chemistry-like terms, it is an irreversible reaction. Once passwords are hashed, you cannot retrieve the original password from the generated string.
+
+A commonly used hashing algorithm for passwords is bcrypt. In Node.js projects, developers often install it using:
+npm install bcrypt
+
+Example:
+password123 → ef92b778bafe771e89245b89ecbc08a4…
+
+Here, your password is stored as this generated string in the database, and the original password password123 is never saved.
+
+During login:
+-The user enters their password.  
+-The system hashes it again.  
+-The new hash is compared with the stored hash.  
+-If they match, the password is correct.  
+
+That is why when recovering passwords, websites never say:
+"Oh! Hi, you forgetful little thing, here’s your password: password123."
+
+Instead, they redirect you to your email and ask you to reset a new password. Long story short, if you forget password123, there is no way to retrieve it, only to reset it.
+
+Even I, as the developer, cannot see the user’s password and I can only authenticate the user if their generated hash matches the one in the database.
+
+But what if two or more people have the same password, like password123?
+
+This is where salts come in.
+
+Salts are random strings mixed with the password before hashing:
+
+password123 + random_salt → hash
+
+So for testing purposes, all the users in my database have the same password so I can log in easily without remembering different passwords. But if you notice even though the passwords are the same, their hashed strings are different. That happens because of salts.
+
+
+# Encryption
+
+Have you ever opened WhatsApp and seen the message: End-to-end encrypted? Or maybe you’ve seen the famous WhatsApp advertisement explaining how your data stays private.
+
+Encryption is used when data must remain confidential but still retrievable.You cannot use hashing here because when you send a message, it must be readable on the other end. Unlike hashing, encrypted data can be reversed if the correct key is available. Hence encryption is used here.
+
+Encryption transforms readable data into an unreadable format using a secret key and only someone with the correct key can decrypt the data.
+
+All kinds of media, text, images, files, PDFs and even calls can be encrypted.
+
+Encryption is commonly used for:
+-Securing messages  
+-Protecting sensitive files  
+-Safeguarding data in databases  
+-Securing communication over HTTPS  
+
+For example, Transport Layer Security (TLS) encrypts communication between browsers and servers. If you ever visit a website and see the padlock icon in your browser, it means TLS is being used to secure the connection. Most websites today use TLS.
+
+
+To conclude
+
+Encoding helps format data, hashing protects passwords, and encryption ensures confidentiality. Together, these techniques form the foundation of modern security systems used across the internet. Knowing these concepts is essential because user data needs to remain protected even if the database is compromised.
 `
-  },
-
-  {
+},
+  
+{
     id: 5678,
     slug: "the-first-time-deployment-fought-back",
     title: "The First Time Deployment Fought Back",
@@ -85,6 +145,42 @@ This wasn’t just a performance issue. It was my introduction to deployment: co
 
 I’m still learning this side of development, but that’s part of the process. Sometimes, the most useful lessons show up after you've hit deploy.
     `
+  },
 
+  {
+    id: 1234,
+    slug: "a-blog-about-a-blog",
+    title: "A blog about a blog",
+    subtitle: "Because...why not?",
+    date: "August 21, 2025",
+    content: `
+I’d always wanted to start a blog. Lousy Writers first began as a WordPress site, but I soon discovered that I could build a website myself, and since it’s now part of my curriculum, that turned out to be a win-win situation. So, I began learning web technologies to create this website. Along the way, I discovered that there are two aspects of web development: frontend and backend. Most people begin with frontend, which is about how the website looks and how users interact with it (excluding functionality, which is essentially part of backend development).
+
+-> Design, Setup, and Tailwind
+I started by designing the basic layout of the website for both mobile and desktop views. You know, there’s something deeply calming about fine-tuning a design down to the very last pixel. I had studied plain CSS earlier, and I decided to learn Tailwind this time.
+
+And thus began the flood of npm commands — and the inevitable “command not found” errors — during the Tailwind installation. Not to mention the bulky and intimidating node_modules folder.
+
+I worked through the Tailwind docs, experimenting with CSS properties until I felt comfortable. Then I coded the homepage in desktop view, only to realize later that Tailwind is built on a mobile-first philosophy. You’re supposed to code the mobile version first, then tweak it for larger screens using responsive variants like sm:, md:, and lg:. So I had to redesign it, and that was a lesson learned the hard way.
+
+-> React, Components, and Routing
+After getting comfortable with styling, the next step was learning how to build reusable components instead of rewriting the same code repeatedly. This meant shifting from blindly typing out code to thinking carefully about whether a div could be reused, how it would fit into the component hierarchy, and which parts should stay consistent versus which parts needed to change.
+
+For example, the blog cards on this site share the same structure and design, but the actual content - title, subtitle, and dates differ. That’s where Props come in and they allow you to pass data into components so they stay reusable. It’s similar to writing a function and then calling it with different arguments. 
+
+On the other hand, when data changes inside a component (like the value of a counter or a like button), that’s handled with State. State is like the memory of a component it stores values that can change over time as the user interacts with it.
+
+To set up routing and dynamic URLs, I used React Router Dom. Notice how the page doesn’t reload when you navigate to another page? That’s because React Router DOM enables client-side routing in single-page applications, which allows smoother user interactions and faster loading times. 
+
+In short, I really appreciate how thoughtfully React was designed — a simple yet powerful tool.
+
+-> Storage, GitHub, and Deployment
+Currently, as I write this, the blog contents are saved in local storage files. But I do have a few options in mind that I can later switch to, partly because the formatting of the blog content on this page that you're reading can be improved. Not to mention the possibility of adding images in the future. 
+
+Learning Git and GitHub through this project has been incredibly fruitful. I've gained a practical understanding of what each command does, and more importantly, the 'why' behind using them. When you first learn about them, they all feel so theoretical and abstract, like "What do you even mean I've got a merge conflict from GitHub?!" The only conflict I sensed was cognitive, trying to merge these new concepts into my understanding! But all of that eventually got better.
+
+I thought deploying your website had a steep learning curve and would take a lot of time, and once you've deployed, nothing could be changed. But I was glad to be mistaken. One of my friends told me deploying on Vercel takes up no more than 5 minutes, since it pulls the code directly from your GitHub repository.  So — dramatic pause — I finally deployed the project and opened the link on my phone. That moment felt really great (after fixing a few responsiveness issues, obviously). But in hindsight, I think that’s development in a nutshell: thinking you’re completely stuck until suddenly, you’re not.
+`
   }
+
 ]; 
